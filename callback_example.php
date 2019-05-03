06cddb2e5c33baddc5cef6bf92120fb90801bfac
Kayky Ramos <kayky@me.com>
2019-05-03 17:48:34 -0300

    Update callback_example.php

--- callback_example.php
+++ callback_example.php
@@ -1,18 +1,17 @@
 <?php
-
 	include "LeadBorg.class.php";
 	
-    $robot = new LeadBorg("/*Paste your token here*/");
+	$robot = new LeadBorg("/*Paste your token here*/");
 
-    $robot->onReceiveMessage(function($that, $message){
-        switch ($message->type) {
-            case 'chat':
-                sleep(rand(5,10));
-                $that->sendMessage($message->chatId->user, $message->content); //echo the message
-                break;
+	$robot->onReceiveMessage(function($that, $message){
+        	switch ($message->type) {
+            		case 'chat':
+                		sleep(rand(5,10));
+                		$that->sendMessage($message->chatId->user, $message->content); //echo the message
+                		break;
 
-            case 'ptt':
-                $that->sendMessage($message->chatId->user, "I can't hear you now... 🙉"));
-                break;
-        }
-    });
+            		case 'ptt':
+                		$that->sendMessage($message->chatId->user, "I can't hear you now... 🙉"));
+                		break;
+        	}
+	});
