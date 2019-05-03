dede6c80e294d5343327de3522d9b92d87d48564
Kayky Ramos <kayky@me.com>
2019-05-03 13:27:37 -0300

    Update LeadBorg.class.php

--- LeadBorg.class.php
+++ LeadBorg.class.php
@@ -1,103 +1,111 @@
 <?php
-    class LeadBorg{
-	    public $TOKEN;
-	    public $DEBUG;
-	    public $DATA;
+class LeadBorg
+{
+	public $TOKEN;
+	public $DEBUG;
+	public $DATA;
 
-	    const API_URL      = "https://api.leadb.org:9090";
-	    const SEEN         = "seen";
-	    const SEND_MESSAGE = "sendmessage";
-	    const SEND_MEDIA   = "sendmedia";
-	    const GET_UNREAD   = "getunread";
-	    const GET_ALL      = "getall";
-	    const GET_HISTORY  = "gethistory";
-	    const PHONE_DOMAIN = "@c.us";
-	    const GROUP_DOMAIN = "@g.us";
+	const API_URL      = "https://api.leadb.org:9090";
+	const SEEN         = "seen";
+	const SEND_MESSAGE = "sendmessage";
+	const SEND_MEDIA   = "sendmedia";
+	const GET_UNREAD   = "getunread";
+	const GET_ALL      = "getall";
+	const GET_HISTORY  = "gethistory";
+	const PHONE_DOMAIN = "@c.us";
+	const GROUP_DOMAIN = "@g.us";
 
-	    public function __construct(string $token, bool $debug = false){
-			$this->TOKEN = $token;
-            $this->DEBUG = $debug;
-            return $this;
+	public function __construct(string $token, bool $debug = false)
+	{
+		$this->TOKEN = $token;
+		$this->DEBUG = $debug;
+		return $this;
+	}
+
+	public function createUrl(string $method, array $args = [])
+	{
+		$args['token'] = $this->TOKEN;
+		return self::API_URL . '/?cmd=' . $method . '&' . http_build_query($args);
+	}
+
+	public function query(string $method, array $args)
+	{
+		$url = $this->createUrl($method, $args);
+
+		if ($this->DEBUG) {
+			print("[DEBUG]: \n");
+			print("Requested URL: " . $url . "\n");
 		}
 
-	    public function createUrl(string $method, array $args = []){
-            $args['token'] = $this->TOKEN;
-            return self::API_URL.'/?cmd='.$method.'&'.http_build_query($args);
-        }
+		return file_get_contents($url);
+	}
 
-	    public function query(string $method, array $args){
-            $url = $this->createUrl($method, $args);
+	public function sendMessage(string $chatId, string $message)
+	{
+		if (strpos($chatId, '-') !== false)
+			$chatId .= self::GROUP_DOMAIN;
+		else
+			$chatId .= self::PHONE_DOMAIN;
 
-            if ($this->DEBUG){
-                print("[DEBUG]: \n");
-                print("Requested URL: ".$url."\n");
-            }
+		$response = json_decode($this->query(self::SEND_MESSAGE, ['chatId' => $chatId, 'content' => $message]));
 
-            return file_get_contents($url);
-        }
+		if ($this->DEBUG) {
+			print("[DEBUG]: \n");
+			var_dump($response);
+		}
 
-        public function sendMessage(string $chatId, string $message){
-            if (strpos($chatId, '-') !== false)
-                $chatId .= self::GROUP_DOMAIN;
-            else
-                $chatId .= self::PHONE_DOMAIN;
+		if ($response->status == "OK")
+			return true;
+		else
+			return false;
+	}
 
-            $response = json_decode($this->query(self::SEND_MESSAGE, ['chatId' => $chatId, 'content' => $message]));
+	public function sendMedia(string $chatId, string $path, string $caption)
+	{
+		if (strpos($chatId, '-') !== false)
+			$chatId .= self::GROUP_DOMAIN;
+		else
+			$chatId .= self::PHONE_DOMAIN;
 
-        	if ($this->DEBUG){
-            	print("[DEBUG]: \n");
-            	var_dump($response);
-            }
+		$response = json_decode($this->query(self::SEND_MEDIA, ['chatId' => $chatId, 'filePath' => $path, 'caption' => $caption]));
 
-            if ($response->status == "OK")
-            	return true;
-            else
-            	return false;
-        }
+		if ($this->DEBUG) {
+			print("[DEBUG]: \n");
+			var_dump($response);
+		}
 
-        public function sendMedia(string $chatId, string $path, string $caption){
-            if (strpos($chatId, '-') !== false)
-                $chatId .= self::GROUP_DOMAIN;
-            else
-                $chatId .= self::PHONE_DOMAIN;
-            
-        	$response = json_decode($this->query(self::SEND_MEDIA, ['chatId' => $chatId, 'filePath' => $path, 'caption' => $caption]));
+		if ($response->status == "OK")
+			return true;
+		else
+			return false;
+	}
 
-        	if ($this->DEBUG){
-            	print("[DEBUG]: \n");
-            	var_dump($response);
-            }
+	public function sendSeen(string $chatId)
+	{
+		if (strpos($chatId, '-') !== false)
+			$chatId .= self::GROUP_DOMAIN;
+		else
+			$chatId .= self::PHONE_DOMAIN;
 
-            if ($response->status == "OK")
-            	return true;
-            else
-            	return false;
-        }
+		$response = json_decode($this->query(self::SEEN, ['chatId' => $chatId]));
 
-        public function sendSeen(string $chatId){        
-            if (strpos($chatId, '-') !== false)
-                $chatId .= self::GROUP_DOMAIN;
-            else
-                $chatId .= self::PHONE_DOMAIN;
-    
-        	$response = json_decode($this->query(self::SEEN, ['chatId' => $chatId]));
+		if ($this->DEBUG) {
+			print("[DEBUG]: \n");
+			var_dump($response);
+		}
 
-        	if ($this->DEBUG){
-            	print("[DEBUG]: \n");
-            	var_dump($response);
-            }
+		if ($response->status == "OK")
+			return true;
+		else
+			return false;
+	}
 
-            if ($response->status == "OK")
-            	return true;
-            else
-            	return false;
-        }
-
-        public function onReceiveMessage($callback){
-            if ($_GET['token'] == $this->TOKEN){
-                $data = json_decode(base64_decode($_GET['data']));
-                self::sendSeen($data->chatId->user);
-                $callback($this, $data);
-            }
-        }
-    }
+	public function onReceiveMessage($callback)
+	{
+		if ($_GET['token'] == $this->TOKEN) {
+			$data = json_decode(base64_decode($_GET['data']));
+			self::sendSeen($data->chatId->user);
+			$callback($this, $data);
+		}
+	}
+}
